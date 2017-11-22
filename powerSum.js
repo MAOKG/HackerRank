989cf5e4c58fce37faa340be9e1fb6c206acd5df
Maosen <maosen1993@gmail.com>
2017-11-22 13:39:18 -0500

    power Sum

--- powerSum.js
+++ powerSum.js
@@ -1,15 +1,14 @@
 function processData(input) {
     //Enter your code here
     const array = input.split('\n');
-    const X = array[0];
-    const N = array[1];
+    const X = Number(array[0]);
+    const N = Number(array[1]);
 
     function powerSum(curr, target) {
-        if (target === 0) return 1;
+        if (target === 0 || Math.pow(curr, N) === target) return 1;
         if (Math.pow(curr, N) > target) return 0;
-        // if (target - Math.pow(curr, N) <= curr) return 0;
         let sum = 0;
-        for (let i = curr; i <= Math.floor(Math.pow(target, 1 / N)); i++) {
+        for (let i = curr; i <= Math.pow(target, 1 / N); i++) {
             sum += powerSum(i + 1, target - Math.pow(i, N));
         }
         return sum;
